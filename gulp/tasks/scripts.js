d551af512ecb24778f2ac987c7f399bd2b09a627
Ahmad <ahmadzeinab118@gmail.com>
2019-06-10 20:54:12 +0200

    Completed

---
+++ gulp/tasks/scripts.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp'),
+webpack = require('webpack');
+
+function a(){
+  webpack(require('../../webpack.config.js'),b);
+}
+function b(error,status){
+  if(error)
+  {
+    console.log(error.toString());
+  }
+console.log(status.toString());
+
+}
+
+gulp.task('scripts',a);
